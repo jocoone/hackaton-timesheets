59f9f11e75b861630dbfcde0745084599a02b8f5
AXXES\svandenakkerveken <sam.vandenakkerveken@axxes.com>
2017-09-01 14:21:49 +0200

    Date Range fix

--- timesheet-backend/src/main/java/be/axxes/hackaton/timesheets/util/DateRange.java
+++ timesheet-backend/src/main/java/be/axxes/hackaton/timesheets/util/DateRange.java
@@ -1,4 +1,38 @@
 package be.axxes.hackaton.timesheets.util;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateRange {
+
+    private final Date fromDate;
+    private final Date toDate;
+
+    private DateRange(Date fromDate, Date toDate) {
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public Date getToDate() {
+        return toDate;
+    }
+
+    public static DateRange forWeek(int year, int weekNumber) {
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setWeekDate(year, weekNumber, Calendar.MONDAY);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        Date dateBeginWeek = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 7);
+        Date dateEndWeek = calendar.getTime();
+
+        return  new DateRange(dateBeginWeek, dateEndWeek);
+    }
 }
