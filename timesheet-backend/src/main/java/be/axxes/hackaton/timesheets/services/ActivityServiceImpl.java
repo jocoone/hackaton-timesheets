f59dab4d405798466061d7488dbc363db14a9c3c
selske <sels.kevin@gmail.com>
2017-09-01 16:35:28 +0200

    added at j fb

--- timesheet-backend/src/main/java/be/axxes/hackaton/timesheets/services/ActivityServiceImpl.java
+++ timesheet-backend/src/main/java/be/axxes/hackaton/timesheets/services/ActivityServiceImpl.java
@@ -1,26 +1,26 @@
 package be.axxes.hackaton.timesheets.services;
 
-import java.util.Date;
-
-import org.springframework.stereotype.Service;
-
 import be.axxes.hackaton.timesheets.dao.ActivityDao;
 import be.axxes.hackaton.timesheets.model.Activity;
-import be.axxes.hackaton.timesheets.model.ActivityType;
 import be.axxes.hackaton.timesheets.model.User;
 import be.axxes.hackaton.timesheets.util.DateRange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
-public class ActivityServiceImpl implements ActivityService  {
-	private ActivityDao activityDao;
+public class ActivityServiceImpl implements ActivityService {
+    @Autowired
+    private ActivityDao activityDao;
 
-	@Override
-	public Iterable<Activity> getActivities(User user, DateRange range) {
-		Date from = range.getFromDate();
-		Date to = range.getToDate();
-		return activityDao.findByDateBetween(from, to);
-	}
-	
+    @Override
+    public Iterable<Activity> getActivities(final User user, final DateRange range) {
+        Date from = range.getFromDate();
+        Date to = range.getToDate();
+        return activityDao.findByDateBetween(from, to);
+    }
+
     @Override
     public Activity create(final Activity activity) {
         return activityDao.save(activity);
