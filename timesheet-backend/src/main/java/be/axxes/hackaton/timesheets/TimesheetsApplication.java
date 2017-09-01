e4e6d90632d44823f79e0d21f0c6cbb6cc800ed4
Joey Comhaire <joey.comhaire@gmail.com>
2017-09-01 10:36:02 +0200

    initial commit

---
+++ timesheet-backend/src/main/java/be/axxes/hackaton/timesheets/TimesheetsApplication.java
@@ -0,0 +1,12 @@
+package be.axxes.hackaton.timesheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetsApplication.class, args);
+	}
+}
